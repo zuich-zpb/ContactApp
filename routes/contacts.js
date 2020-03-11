758bf77d9c9361b80037af2bde33c3f0b67cc60f
Zuich <zuich-zpb@tut.by>
2020-03-12 01:56:56 +0300

    Second commit

--- routes/contacts.js
+++ routes/contacts.js
@@ -72,8 +72,7 @@ router.put('/:id',auth, async (req,res)=>{
 
     try{
 let contact=await Contact.findById(req.params.id)
-if(!contact)
-    return res.status(404).json({msg:"Contact not found"});
+if(!contact) return res.status(404).json({msg:"Contact not found"});
     //Make sure user owns contact
     if(contact.user.toString() !==req.user.id){
         return res.status(401).json({msg:'Not autorized'})
@@ -92,8 +91,21 @@ res.status(500).send("Server Error");
 //@route DELETE api/contacts/:id
 //@desc   Deletecontact
 //@access  Private
-router.delete('/:id',(req,res)=>{
-    res.send('Delete contact');
+router.delete('/:id',auth,async(req,res)=>{
+    try{
+        let contact=await Contact.findById(req.params.id)
+        if(!contact) return res.status(404).json({msg:"Contact not found"});
+            //Make sure user owns contact
+            if(contact.user.toString() !==req.user.id){
+                return res.status(401).json({msg:'Not autorized'})
+            }
+            
+        await Contact.findByIdAndRemove(req.params.id)
+            res.json({msg:'Contact removed'});
+            }catch(err){
+        console.error(err.message)
+        res.status(500).send("Server Error");
+            }
 });
 
 
