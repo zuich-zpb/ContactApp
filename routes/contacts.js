86f7b97d57da27fd992e2ca45e17d02e82cb7a2e
Zuich <zuich-zpb@tut.by>
2020-03-18 23:22:22 +0300

    Deploy

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,9 +1,9 @@
 const express=require("express");
 const router = express.Router();
 const auth=require('../middleware/auth')
-const {check,validationResult}=require('express-validator/check')
+const {check,validationResult}=require('express-validator')
 
-const User=require('../models/user');
+const User=require('../models');
 const Contact=require('../models/contact');
 
 //@route GET api/contacts
