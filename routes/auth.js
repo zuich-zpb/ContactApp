9dd9698417eb48f1aafdf8ee89de1521ea175337
Zuich <zuich-zpb@tut.by>
2020-03-18 23:58:44 +0300

    Deploy

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ const jwt=require("jsonwebtoken");
 const config = require('config');
 const auth=require('../middleware/auth');
 
-const {check,validationResult}=require('express-validator/check')
+const {check,validationResult}=require('express-validator')
 
 const User=require('../models/user');
 
--- routes/contacts.js
+++ routes/contacts.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const auth=require('../middleware/auth')
 const {check,validationResult}=require('express-validator')
 
-const User=require('../models');
+
 const Contact=require('../models/contact');
 
 //@route GET api/contacts
