c089c30a0964275c463a714b81e9b09917905976
Zuich <zuich-zpb@tut.by>
2020-03-25 21:14:35 +0300

    deploy

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-
-
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
