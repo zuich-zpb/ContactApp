0cdfe504241c1445f41cba105de470795d1aba02
Zuich <zuich-zpb@tut.by>
2020-03-26 01:19:59 +0300

    first commit

--- client/src/components/context/alert/AlertState.js
+++ client/src/components/context/alert/AlertState.js
@@ -1,9 +1,8 @@
 import React, {useReducer} from 'react';
-import { v4 as uuidv4 } from 'uuid'
-
+import {v4 as uuidv4} from 'uuid';
 import AlertContext from './alertContext';
-import alertReducer from './alertReducer.js';
-import {SET_ALERT,REMOVE_ALERT} from '../types';
+import alertReducer from './alertReducer';
+import {SET_ALERT, REMOVE_ALERT} from '../types';
 
 const AlertState = props => {
   const initialState = [];
